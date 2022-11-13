aab6e3b6b279d58efd85133bcb74f24150a97b93
Nishiumi_Daina <71867595+nishiumidaina@users.noreply.github.com>
2022-11-14 01:49:09 +0900

    ホワイトリストのルーティングテスト用に駐輪場情報を返すAPIを設置。

--- src/app/admin-server/app/Http/Controllers/Spot/SpotController.php
+++ src/app/admin-server/app/Http/Controllers/Spot/SpotController.php
@@ -4,8 +4,14 @@ namespace App\Http\Controllers\Spot;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Spot;
 
 class SpotController extends Controller
 {
-    //
+    public function spotAdminGet()
+    {
+        $spot = Spot::get();
+
+        return $spot;
+    }
 }
--- src/app/admin-server/routes/api.php
+++ src/app/admin-server/routes/api.php
@@ -1,9 +1,9 @@
 <?php
-
+ 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Spot\SpotController;
-
+ 
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -14,9 +14,16 @@ use App\Http\Controllers\Spot\SpotController;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-
+ 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/spot_admin_get/{id}', [SpotController::class, 'spotAdminGet']);
+// ホワイトリストを設けてアクセスできるIPを制御する。
+Route::middleware(['firewall'])->group(function () {
+    Route::get('/', function () {
+        return view('welcome');
+    });
+
+    Route::get('/spot_admin_get/{id}', [SpotController::class, 'spotAdminGet']);
+});
