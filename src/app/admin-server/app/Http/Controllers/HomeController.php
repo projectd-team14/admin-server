fbcb38d8ebf5b7972067494454402665ddf86c29
Nishiumi_Daina <71867595+nishiumidaina@users.noreply.github.com>
2022-12-03 05:32:33 +0900

    ダッシュボードを追加

--- src/app/admin-server/app/Http/Controllers/HomeController.php
+++ src/app/admin-server/app/Http/Controllers/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Spot;
 
 class HomeController extends Controller
 {
@@ -24,7 +25,56 @@ class HomeController extends Controller
     public function index()
     { 
         $user = \Auth::user();
-        return view('layouts/app', compact('user'));
+        $spot = Spot::where('spots_count_month1', '<>', 'None')->where('spots_violations', '<>', 'None')->get(['spots_id', 'spots_name', 'spots_url', 'spots_latitude', 'spots_longitude', 'spots_address', 'spots_count_month1', 'spots_violations']);
+
+        $spotsCountMonth = [];
+        $spotsCountViolations = [];
+
+        for ($i = 0; $i < count($spot); $i++) {
+            $monsList = explode(',', $spot[$i]['spots_count_month1']);
+            $violationsList = explode(',', $spot[$i]['spots_violations']);
+
+            array_push($spotsCountMonth, $monsList);
+            array_push($spotsCountViolations, $violationsList);                
+        }
+        
+        $countMonthAll = [];
+        $countViolationsAll = [];
+
+        for ($i = 0; $i < 30; $i++) {
+            $countMonth = 0;
+
+            for ($i2 = 0; $i2 < count($spotsCountMonth); $i2++) {
+                $countMonth = $countMonth + (int)$spotsCountMonth[$i2][$i];
+            }
+
+            array_push($countMonthAll, $countMonth);
+        }
+
+        for ($i = 0; $i < 30; $i++) {
+            $countViolations = 0;
+
+            for ($i2 = 0; $i2 < count($spotsCountViolations); $i2++) {
+                $countViolations = $countViolations + (int)$spotsCountViolations[$i2][$i];
+            }
+            
+            array_push($countViolationsAll, $countViolations);
+        }
+
+        $labels = [];
+
+        for ($i = 0; $i < count($countViolationsAll); $i++) {
+            $objDateTime = date('Y-m-d', strtotime("-$i day"));
+            array_push($labels, $objDateTime);
+        }
+
+        $data = [
+            'labels' => $labels,
+            'month_list' => $countMonthAll,
+            'violation_list' => $countViolationsAll
+        ];
+
+        return view('layouts/app', compact('user', 'spot', 'data'));
     }
 
     public function top()
--- src/app/admin-server/resources/views/home.blade.php
+++ src/app/admin-server/resources/views/home.blade.php
@@ -1,4 +1,4 @@
-<div class="top">
+<div>
     <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
         <div class="container">
             <a class="navbar-brand" href="{{ url('/') }}">
@@ -57,8 +57,9 @@
     <div id="chart_card" class="row justify-content-center">
         <div class="col-12">
             <div class="card">
-                <div class="card-name card-header">放置自転車推移-全国</div> 
-                    <div class="my-3 mx-5">
+                <div class="card-name card-header">自転車利用推移-全国</div> 
+                    <div class="my-3 mx-5" style="height: 300px;">
+                    <canvas id="ex_chart" style="max-height: 300px;"></canvas>
                     <div class="row">
                     </div>
                 </div>
@@ -71,7 +72,33 @@
         <div class="col-3">
             <div class="card">
                 <div class="card-name card-header">駐輪場一覧</div> 
-                    <div class="my-3 mx-5">
+                    <div class="overflow-auto" style="height: 400px;">
+                    <table id="perfume" class="table">
+                    <thead>
+                        <tr>
+                        <th class="text-center" scope="col">駐輪場名</th>
+                        <th class="text-center" scope="col">OFF-ON</th>
+                        </tr>
+                    </thead>
+                    @php
+                        $count = 0;
+                    @endphp
+                        @if(isset($spot))
+                            @foreach($spot as $spots)
+                            <tr>
+                                <td class="text-center">{{ $spots['spots_name'] }}</td>
+                                <td class="text-center">
+                                    <div class="form-switch">
+                                        <input class="form-check-input" type="checkbox" id="flexSwitchCheckDefault" value="{{ $spots['spots_id'] }}" onclick="onClickSpotChart({{ $count }})"> 
+                                    </div>
+                                </td>
+                            </tr>
+                            @php
+                                $count = $count + 1;
+                            @endphp
+                            @endforeach 
+                        @endif
+                    </table>                    
                     <div class="row">
                     </div>
                 </div>
@@ -80,7 +107,8 @@
         <div class="col-9">
             <div class="card">
                 <div class="card-name card-header">駐輪場マップ</div> 
-                    <div class="my-3 mx-5">
+                    <div style="height: 400px;">
+                        <div id="mapid" style="height: 400px;"></div>
                     <div class="row">
                     </div>
                 </div>
@@ -88,3 +116,112 @@
         </div>  
     </div>
 </div>
+
+<link rel="stylesheet" href="https://unpkg.com/leaflet@1.6.0/dist/leaflet.css"
+   integrity="sha512-xwE/Az9zrjBIphAcBb3F6JVqxf46+CDLwfLMHloNu6KEQCAWi6HcDUbeOfBIptF7tcCzusKFjFw2yuvEpDL9wQ=="
+   crossorigin=""/>
+ <!-- Make sure you put this AFTER Leaflet's CSS -->
+ <script src="https://unpkg.com/leaflet@1.6.0/dist/leaflet.js"
+   integrity="sha512-gZwIG9x3wUXg2hdXF6+rVkLF/0Vi9U8D2Ntg4Ga5I5BZpVkVxlJWbSQtXPSiUTtC0TjtGOmxa1AJPuV0CPthew=="
+   crossorigin="">
+</script>
+<script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+
+<script>
+    const labels = @json($data['labels']);
+    const countMonthAll = @json($data['month_list']);
+    const countViolationAll = @json($data['violation_list']);
+    const spotData = @json($spot);
+    const datasets =  [
+            {
+                id: 0,
+                label: '平均利用者',
+                data: countMonthAll,
+                borderColor: 'rgba(100, 100, 255, 1)'
+            },
+            {
+                id: 0,
+                label: '合計放置車両',
+                data: countViolationAll,
+                borderColor: 'rgba(255, 100, 100, 1)'
+            }
+        ];
+    var ctx = document.getElementById('ex_chart');
+    var ex_chart;
+
+    // グラフ
+    function createChart() {
+        ex_chart = new Chart(ctx, {
+            type: 'line',
+            data: {
+                labels: labels,
+                datasets: datasets
+            }
+        });
+    }
+
+    function onClickSpotChart(a) {
+        var update = true;
+
+        for (i = 0; i < datasets.length; i++) {
+            if (datasets[i]['id'] === spotData[a]['spots_id']) {
+                datasets.splice(i, 2);
+                update = false;
+            }
+        }
+
+        if (update) {
+            var r = Math.floor( Math.random() * (254 + 1 - 100) ) + 100 ;
+            var g = Math.floor( Math.random() * (254 + 1 - 100) ) + 100 ;
+            var b = Math.floor( Math.random() * (254 + 1 - 100) ) + 100 ;
+
+            var newData = {
+                id: spotData[a]['spots_id'],
+                label: spotData[a]['spots_name'] + '（平均利用者）',
+                data: spotData[a]['spots_count_month1'].split(','),
+                borderColor: 'rgba(' + r + ',' + g +',' + b + ', 1)'
+            }
+
+            datasets.push(newData);
+
+            var r = Math.floor( Math.random() * (254 + 1 - 100) ) + 100 ;
+            var g = Math.floor( Math.random() * (254 + 1 - 100) ) + 100 ;
+            var b = Math.floor( Math.random() * (254 + 1 - 100) ) + 100 ;
+
+            var newData = {
+                id: spotData[a]['spots_id'],
+                label: spotData[a]['spots_name'] + '（合計放置車両）',
+                data: spotData[a]['spots_violations'].split(','),
+                borderColor: 'rgba(' + r + ',' + g +',' + b + ', 1)'
+            }
+
+            datasets.push(newData);
+            ex_chart.update();
+        } else {
+            ex_chart.update();
+        }
+
+    }
+
+    createChart();
+
+    // 地図
+    var map = L.map('mapid', {
+    center: [35.66572, 139.73100],
+    zoom: 10,
+    });
+    var tileLayer = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '© <a href="http://osm.org/copyright">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>',
+    });
+    tileLayer.addTo(map);
+
+    for (i = 0; i < spotData.length; i++) {
+        var marker = L.marker([spotData[i]['spots_latitude'], spotData[i]['spots_longitude']]).addTo(map);
+        var urlReplace =  spotData[i]['spots_url'].replace('watch?v=', 'embed/');
+        var cameraHtml = '<iframe width="560" height="315"src="' + urlReplace + '" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>';
+
+        marker.bindPopup("<div>" + spotData[i]['spots_name'] + "</div>" + 
+        "<div>" + cameraHtml + "</div>", {maxWidth: 560, closeOnClick: true});
+    }
+
+</script>
