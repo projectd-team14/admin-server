4f8ee98acd4ee0bd4e9ea6b02992506bd8426bb7
Nishiumi_Daina <71867595+nishiumidaina@users.noreply.github.com>
2022-12-03 21:37:25 +0900

    サイドバーのレイアウトを修正

--- src/app/admin-server/resources/views/navibar.blade.php
+++ src/app/admin-server/resources/views/navibar.blade.php
@@ -10,45 +10,112 @@
   </head>
   <body>
     <div class="col-md-2 p-0 navigation side-view">
-      <ul>
-        <li class="list">
-          <a href="/home">
-            <span class="icon"><ion-icon name="home-outline"></ion-icon></span>
-            <span class="title">ホーム</span>
-          </a>
-        </li>
-        <li class="list">
-          <a href="/chart">
-            <span class="icon"><ion-icon name="bar-chart-outline"></ion-icon></span>
-            <span class="title">グラフ</span>
-          </a>
-        </li>
-        <li class="list">
-          <a href="/camera">
-          <span class="icon"><ion-icon name="image-outline"></ion-icon></span>
-            <span class="title">カメラ</span>
-          </a>
-        </li>
-        <li class="list">
-          <a href="/download">
+      <div class="d-flex align-items-end position-absolute bottom-0 end-0">
+        <a class="my-5 mx-5 btn btn-primary btn-lg rounded-pill" href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">{{ __('Logout') }}</a>
+        <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+          @csrf
+        </form>
+      </div>
+      <div class="accordion accordion-flush" id="accordionFlushExample">
+        <div class="accordion-item navigation">
+          <h2 class="accordion-header navbar" id="flush-headingOne">
+            <button class="accordion-button collapsed navbar text-white" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseOne" aria-expanded="false" aria-controls="flush-collapseOne">
+              <span class="icon"><ion-icon name="home-outline"></ion-icon></span>
+              <span class="title">ホーム</span>
+            </button>
+          </h2>
+          <div id="flush-collapseOne" class="accordion-collapse collapse" aria-labelledby="flush-headingOne" data-bs-parent="#accordionFlushExample">
+            <div class="accordion-body sub">
+              <a href="/home">
+                <div class="text-white">
+                  ・ダッシュボード
+                </div>
+              </a>
+            </div>
+          </div>
+        </div>
+        <div class="accordion-item navigation">
+          <h2 class="accordion-header navbar" id="flush-headingTwo">
+            <button class="accordion-button collapsed navbar text-white" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseTwo" aria-expanded="false" aria-controls="flush-collapseTwo">
+              <span class="icon"><ion-icon name="bar-chart-outline"></ion-icon></span>
+              <span class="title">グラフ</span>
+            </button>
+          </h2>
+          <div id="flush-collapseTwo" class="accordion-collapse collapse" aria-labelledby="flush-headingTwo" data-bs-parent="#accordionFlushExample">
+            <div class="accordion-body sub">
+              <a href="/chart">
+                <div class="text-white">
+                  ・条件検索
+                </div>
+              </a>
+            </div>
+            <div class="accordion-body sub">
+              <a href="/chart">
+                <div class="text-white">  
+                  ・駐輪場分析
+                </div>  
+              </a>
+            </div>
+          </div>
+        </div>
+        <div class="accordion-item navigation">
+          <h2 class="accordion-header navbar" id="flush-headingThree">
+            <button class="accordion-button collapsed navbar text-white" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseThree" aria-expanded="false" aria-controls="flush-collapseThree">
+              <span class="icon"><ion-icon name="image-outline"></ion-icon></span>
+              <span class="title">カメラ</span>
+            </button>
+          </h2>
+          <div id="flush-collapseThree" class="accordion-collapse collapse" aria-labelledby="flush-headingThree" data-bs-parent="#accordionFlushExample">
+            <div class="accordion-body sub">
+              <a href="/camera">
+                <div class="text-white">   
+                  ・条件検索
+                </div>  
+              </a>
+            </div>
+          </div>
+        </div>
+        <div class="accordion-item navigation">
+          <h2 class="accordion-header navbar" id="flush-headingFour">
+            <button class="accordion-button collapsed navbar text-white" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseFour" aria-expanded="false" aria-controls="flush-collapseFour">
             <span class="icon"><ion-icon name="bicycle-outline"></ion-icon></span>
             <span class="title">ダウンロード</span>
-          </a>
-        </li>
-        <li class="list">
-          <a href="/help/{{ $user['id'] }}">
-            <span class="icon"><ion-icon name="help-circle-outline"></ion-icon></span>
-            <span class="title">ヘルプ</span>
-          </a>
-        </li>
-      </ul>
-      <div style="height: 85%;">
-      </div>
-      <div class="text-center">
-          <a class="my-5 mx-5 btn btn-primary btn-lg rounded-pill" href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">{{ __('ログアウト') }}</a>
-          <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-            @csrf
-          </form>
+            </button>
+          </h2>
+          <div id="flush-collapseFour" class="accordion-collapse collapse" aria-labelledby="flush-headingFour" data-bs-parent="#accordionFlushExample">
+            <div class="accordion-body sub">
+              <a href="/download">
+                <div class="text-white">  
+                  ・駐輪場-条件検索-CSV
+                </div>  
+              </a>
+            </div>
+            <div class="accordion-body sub">
+              <a href="/download">
+                <div class="text-white">  
+                  ・グラフ-条件検索-JPG
+                </div>  
+              </a>
+            </div>
+          </div>
+        </div>
+        <div class="accordion-item navigation">
+          <h2 class="accordion-header navbar" id="flush-headingFive">
+            <button class="accordion-button collapsed navbar text-white" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseFive" aria-expanded="false" aria-controls="flush-collapseFive">
+            <span class="icon"><ion-icon name="people-outline"></ion-icon></span>
+            <span class="title">ユーザー</span>
+            </button>
+          </h2>
+          <div id="flush-collapseFive" class="accordion-collapse collapse" aria-labelledby="flush-headingFive" data-bs-parent="#accordionFlushExample">
+            <div class="accordion-body sub">
+              <a href="/download">
+                <div class="text-white">  
+                  ・駐輪場管理団体
+                </div>  
+              </a>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   </body>
